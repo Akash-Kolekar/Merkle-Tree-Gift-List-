1b9545e6db8042e2258d510144dfbe5f40696401
Akash-Kolekar <akashkolekar2003@gmail.com>
2023-02-27 12:45:43 +0530

    Merkle Root Hash

--- client/index.js
+++ client/index.js
@@ -1,14 +1,29 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = "http://localhost:1225";
+
+const merkleTree = new MerkleTree(niceList);
+
+const root = merkleTree.getRoot();
+console.log("root", root);
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // TODO: how do we prove to the server we're on the nice list?
+
+  const name = "Dr. Olga Kassulke";
+  console.log("name", name);
+  const index = niceList.findIndex((n) => n === name);
+  console.log("index", index);
+
+  const proof = merkleTree.getProof(index);
+  console.log("proof:", proof);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    proof: proof,
+    name: name,
   });
 
   console.log({ gift });
